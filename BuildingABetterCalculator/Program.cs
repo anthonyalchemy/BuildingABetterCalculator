5686fbcedd72d5cf1968cc4e700d7c1e62343f7e
anthonyalchemy <anthony@alchemytechs.com>
2019-07-31 09:55:31 +0100

    Add project files.

---
+++ BuildingABetterCalculator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace BuildingABetterCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());//Have to use convert.toDouble because console.Readline expects a string
+
+            Console.Write("Enter a operator: ");
+            string op = Console.ReadLine();
+
+            Console.Write("Enter a number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (op == "+")
+            {
+                Console.WriteLine(num1 + num2);
+
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
